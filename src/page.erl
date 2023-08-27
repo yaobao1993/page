b0f4a64ce51660633144bd68161a4342ab50ddd3
Yao Bao <free7by@163.com>
2023-08-27 23:43:06 +0800

    Update src

--- src/page.erl
+++ src/page.erl
@@ -3,9 +3,9 @@
 -export([init/0]).
 
 init() ->
-    prepare_third_code().
+    add_third_code().
 
-prepare_third_code() ->
+add_third_code() ->
     code:add_path("../third/cowboy-2.9.0/deps/cowlib/ebin"),
     code:add_path("../third/cowboy-2.9.0/deps/ranch/ebin"),
     code:add_path("../third/cowboy-2.9.0/ebin"),
