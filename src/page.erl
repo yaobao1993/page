2b634eb1c87aa7c2d09b6d6aec9bdd36f0fd8cce
Yao Bao <free7by@163.com>
2023-08-28 00:01:12 +0800

    Update src

--- src/page.erl
+++ src/page.erl
@@ -6,8 +6,9 @@ init() ->
     add_third_code().
 
 add_third_code() ->
-    code:add_path("../third/cowboy-2.9.0/deps/cowlib/ebin"),
-    code:add_path("../third/cowboy-2.9.0/deps/ranch/ebin"),
-    code:add_path("../third/cowboy-2.9.0/ebin"),
-    code:add_path("../third/gun-1.3.3/ebin"),
+    Third_path = "/Users/by/start/page/third",
+    code:add_path(Third_path ++ "/cowboy-2.9.0/deps/cowlib/ebin"),
+    code:add_path(Third_path ++ "/cowboy-2.9.0/deps/ranch/ebin"),
+    code:add_path(Third_path ++ "/cowboy-2.9.0/ebin"),
+    code:add_path(Third_path ++ "/gun-1.3.3/ebin"),
     ok.
