f06867686519e668d3462f0e275c62673cf10524
Yao Bao <free7by@163.com>
2023-12-10 22:58:40 +0800

    Update page routes

--- src/page_routes.erl
+++ src/page_routes.erl
@@ -3,10 +3,9 @@
 -export([init/2]).
 
 init(Req, State) ->
+    [Page_user_module] = State,
     Status = 200,
-    Headers = #{
-            <<"content-type">> => <<"text/plain">>
-        },
-    Body = <<"Hello!">>,
+    Headers = Page_user_module:get_res_headers(),
+    Body = Page_user_module:get_res_body(),
     Req2 = cowboy_req:reply(Status, Headers, Body, Req),
     {ok, Req2, State}.
--- src/page_server.erl
+++ src/page_server.erl
@@ -1,15 +1,13 @@
 -module(page_server).
 
--export([start/0, stop/0]).
+-export([start/1, stop/0]).
 
-start() ->
-    start(4000).
-
-start(Port) ->
+start(Config) ->
+    #{port := Port, page_user_module := Page_user_module} = Config,
     {ok, _} = application:ensure_all_started(cowboy),
     Routes = [
             {"/static/[...]", cowboy_static, {dir, "../static"}},
-            {"/[...]", page_routes, []}
+            {"/[...]", page_routes, [Page_user_module]}
         ],
     Dispatch = cowboy_router:compile([{'_', Routes}]),
     cowboy:start_clear(http, [{port, Port}],
