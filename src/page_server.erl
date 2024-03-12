0470965b03843c6950e813c4c6e84a24309f240e
Yao Bao <free7by@163.com>
2024-03-13 00:19:16 +0800

    Update page server

--- src/page_server.erl
+++ src/page_server.erl
@@ -6,12 +6,15 @@ start(Config) ->
     #{port := Port, page_user_module := Page_user_module} = Config,
     {ok, _} = application:ensure_all_started(cowboy),
     Routes = [
-            {"/static/[...]", cowboy_static, {dir, "../static"}},
-            {"/[...]", page_routes, [Page_user_module]}
-        ],
+        {"/static/[...]", cowboy_static, {dir, "../static"}},
+        {"/[...]", page_routes, [Page_user_module]}
+    ],
     Dispatch = cowboy_router:compile([{'_', Routes}]),
-    cowboy:start_clear(http, [{port, Port}],
-        #{env => #{dispatch => Dispatch}}),
+    cowboy:start_clear(
+        http,
+        [{port, Port}],
+        #{env => #{dispatch => Dispatch}}
+    ),
     ok.
 
 stop() ->
